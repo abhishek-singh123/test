3191a6b8c23e44def9680fb455d7d30ce70794f8
singhabhii216@gmail.com <s@gmail.com>
2023-05-24 17:32:14 +0530

    commit on 2405

--- newscript3.sh
+++ newscript3.sh
@@ -6,8 +6,9 @@ git for-each-ref --format='%(committerdate:short) %(refname:lstrip=3) %(refname:
     # protected branch
     if [[ $branch =~ ^master$|^HEAD$ ]]; then
         printf "%9s | %s | %50s | %s\n" "PROTECTED" $date $branch $remote_branch
-    elif [[ "$date" < "May 19, 2023" ]]; then
+    if [[ $branch =~ ^develop$|^HEAD$ ]]; then
+        printf "%9s | %s | %50s | %s\n" "PROTECTED" $date $branch $remote_branch
+elif [[ "$date" < "May 19, 2023" ]]; then
         printf "%9s | %s | %50s | %s\n" "DELETE" $date $branch $remote_branch
-        git push $REMOTE_NAME --delete $branch
+        #git push $REMOTE_NAME --delete $branch
     fi
-done
